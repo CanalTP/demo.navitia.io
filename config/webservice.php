113d324baf2cf0069e0a3beacade7fca64dc2091
Thomas Noury <thomas.noury@canaltp.fr>
2014-06-27 13:39:05 +0200

    Updated webservice config file for easier configuration

--- config/webservice.php
+++ config/webservice.php
@@ -1,9 +1,9 @@
 <?php
 
 $config = array(
-    'Token' => 'token',
+    'Token' => '<TokenHere>',
     'Url' => array(
-        'Navitia' => 'http://vippriv-jorm-ws.mutu.prod.canaltp.prod/v0/',
-        'CrossDomainNavitia' => '/crossdomain_service.php?url=http://vippriv-jorm-ws.mutu.prod.canaltp.prod/v0/',
+        'Navitia' => '<NavitiaUriHere>',
+        'CrossDomainNavitia' => '/crossdomain_service.php?url=<NavitiaUriHere>',
     ),
 );
