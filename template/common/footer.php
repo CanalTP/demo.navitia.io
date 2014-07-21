adb86cf9822d5f49f5cb292028c85daba3431569
Thomas Noury <thomas.noury@canaltp.fr>
2014-07-21 11:43:36 +0200

    Updated version number in footer

--- template/common/footer.php
+++ template/common/footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="footer_content">
         <p>
-            IHM NV2 - version 0.12.2 "limousin" - &copy; 2012 - 2014 Canal TP
+            IHM NV2 - version 0.12.3 "limousin" - &copy; 2012 - 2014 Canal TP
             <?php if (!isset($_GET['debug']) && config_get('debug', 'DebugLink', 'Active') == 'true') { ?>
                 - <a href="<?php echo $this->Request->getUrl() . config_get('debug', 'DebugLink', 'ParamString'); ?>">Debug</a>
             <?php } ?>
