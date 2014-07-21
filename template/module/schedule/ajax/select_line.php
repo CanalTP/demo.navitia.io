3a512aa8d4e0443c6f2cee026318bba96f396332
Thomas Noury <thomas.noury@canaltp.fr>
2014-07-21 10:54:51 +0200

    Route schedule no line in a network

--- template/module/schedule/ajax/select_line.php
+++ template/module/schedule/ajax/select_line.php
@@ -1,16 +1,20 @@
 <div id="<?php echo $type; ?>_line_container">
     <div class="field_text_container">
         <div class="field_padding">
-            <label>Ligne</label>
-            <select name="line_uri" id="<?php echo $type; ?>_line_selection">
-                <option value=""></option>
-                <?php foreach ($line_list as $line) { ?>
-                    <option value="<?php echo $line->Uri; ?>">
-                        <?php echo $line->Code; ?> :
-                        <?php echo $line->Name; ?>
-                    </option>
-                <?php } ?>
-            </select>
+            <?php if ($line_list != null) { ?>
+                <label>Ligne</label>
+                <select name="line_uri" id="<?php echo $type; ?>_line_selection">
+                    <option value=""></option>
+                    <?php foreach ($line_list as $line) { ?>
+                        <option value="<?php echo $line->Uri; ?>">
+                            <?php echo $line->Code; ?> :
+                            <?php echo $line->Name; ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            <?php } else { ?>
+                Aucune ligne pour ce réseau
+            <?php } ?>
         </div>
         <div class="clear"></div>
     </div>
