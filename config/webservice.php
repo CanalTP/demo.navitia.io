b28fdcaa3fe8b44f8d0fb3c59565b37f615128ef
Thomas Noury <thomas.noury@canaltp.fr>
2014-10-29 10:48:09 +0100

    Removed URLs and keys

--- config/webservice.php
+++ config/webservice.php
@@ -1,9 +1,9 @@
 <?php
 
 $config = array(
-    'Token' => 'cd228d65-bdab-4d87-925f-f0feacb84842',
+    'Token' => '<TokenHere>',
     'Url' => array(
-        'Navitia' => 'http://api.navitia.io/v1/',
-        'CrossDomainNavitia' => '/crossdomain_service.php?url=http://api.navitia.io/v1/',
+        'Navitia' => '<NavitiaUriHere>',
+        'CrossDomainNavitia' => '/crossdomain_service.php?url=<NavitiaUriHere>',
     ),
 );
