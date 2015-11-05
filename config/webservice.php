9dbb6da58761f9f774e4bb60398c99aa8cc020b8
Thomas Noury <thomas.noury@canaltp.fr>
2015-11-05 17:56:00 +0100

    Removed token and url from source

--- config/webservice.php
+++ config/webservice.php
@@ -1,9 +1,9 @@
 <?php
 
 $config = array(
-    'Token' => 'a04d90a7-d5a6-4fb7-89bd-a606e0ae1a48',
+    'Token' => '',
     'Url' => array(
-        'Navitia' => 'http://vippriv-jorm-ws.mutu.prod.canaltp.prod/v1/',
-        'CrossDomainNavitia' => '/crossdomain_service.php?url=http://vippriv-jorm-ws.mutu.prod.canaltp.prod/v1/',
+        'Navitia' => 'http://api.navitia.io/v1/',
+        'CrossDomainNavitia' => '/crossdomain_service.php?url=http://api.navitia.io/v1/',
     ),
 );
